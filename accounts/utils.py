d1d9590d5e0096030b7cce29ad11884d9e938d31
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-03-16 12:54:18 +0530

    made basic registeration form

---
+++ accounts/utils.py
@@ -0,0 +1,3 @@
+from uuid import uuid4
+
+def generate_random_token(): return str(uuid4())
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import accounts.models as models
+from django.contrib import messages
+from django.db.models import Q
+from .utils import generate_random_token
 
 # Create your views here.
 def login_page(r):
@@ -7,10 +10,21 @@ def login_page(r):
 
 def register_page(r):
     if r.method == 'POST':
-        first_name = r.POST.get['first_name']
-        last_name = r.POST.get['last_name']
+        first_name = r.POST.get('first_name')
+        last_name = r.POST.get('last_name')
         email = r.POST.get('email')
+        phone_number = r.POST.get('phone_number')
         pass1 = r.POST.get('password1')
-        pass2 = r.POSt.get('password2')
-
+        pass2 = r.POST.get('password2')
+        if models.HotelUser.objects.filter(Q(email=email) | Q(phone_number=phone_number)).exists():
+            messages.error(r, "Account exists with email or phone number.")
+            return redirect(login_page)
+        if pass1 == pass2:
+            hotel_user = models.HotelUser.objects.create(first_name=first_name, last_name=last_name,
+                                                 email=email, phone_number=phone_number,
+                                                 email_token=generate_random_token(), username=phone_number)
+            hotel_user.set_password(pass1)
+            hotel_user.save()
+            messages.success(r, "Account created successfully.")
+            return redirect(login_page)
     return render(r, "register.html")
