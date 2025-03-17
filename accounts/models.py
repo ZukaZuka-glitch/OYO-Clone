651711e1253fefc686c23e6da0c8116436faaf63
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-03-17 11:34:02 +0530

    DB Migration

--- accounts/models.py
+++ accounts/models.py
@@ -9,13 +9,20 @@ class HotelUser(User):
     otp = models.CharField(max_length=10, null=True, blank=True)
     is_verified = models.BooleanField(default=False)
 
+    class Meta:
+        db_table = 'hotel_user'
+
 
 class HotelVendor(User):
     phone_number = models.CharField(max_length=10, unique=True)
+    business_name = models.CharField(max_length=100)
     profile_picture = models.ImageField(upload_to="profile_pictures", null=True, blank=True)
     email_token = models.CharField(max_length=100, null=True, blank=True)
     otp = models.CharField(max_length=10, null=True, blank=True)
     is_verified = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'hotel_vendor'
 
 
 class Amenities(models.Model):
