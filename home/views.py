1ed103d1eb03bca0b5ca3f6e4a981cf0716cff22
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-03-22 03:21:11 +0530

    Sent context from views to HTML

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from accounts.models import Hotel
 
 # Create your views here.
 def index(r):
-    return render(r, "index.html")
+    return render(r, "index.html", context={"hotels": Hotel.objects.all()[:50]})
