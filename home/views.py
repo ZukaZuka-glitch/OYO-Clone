3865542d4705c3881aeee69e3c3fe2b97efd8176
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-03-22 04:28:22 +0530

    Added logic for sorting

--- home/views.py
+++ home/views.py
@@ -3,4 +3,10 @@ from accounts.models import Hotel
 
 # Create your views here.
 def index(r):
-    return render(r, "index.html", context={"hotels": Hotel.objects.all()[:50]})
+    hotels = Hotel.objects.all()
+    if r.GET.get('search'): hotels = hotels.filter(name__icontains=r.GET.get('search'))
+    if r.GET.get('sort'):
+        sort_by = r.GET.get('sort')
+        if sort_by == 'sort_low': hotels = hotels.order_by('hotel_offer_price')
+        else: hotels = hotels.order_by('-hotel_offer_price')
+    return render(r, "index.html", context={"hotels": hotels[:50]})
