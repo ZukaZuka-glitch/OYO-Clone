368061894fe1e6868f41b5b42cd5d9322b54d96a
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-03-22 19:16:01 +0530

    Added the Booking hotel ot admin pnel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import HotelUser, HotelVendor, Amenities
+from .models import HotelUser, HotelVendor, Amenities, HotelBooking
 
 # Register your models here.
 
 admin.site.register(HotelUser)
 admin.site.register(HotelVendor)
 admin.site.register(Amenities)
+admin.site.register(HotelBooking)
