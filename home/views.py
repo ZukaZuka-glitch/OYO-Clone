6b4543ec9c73ad3b7b1065fe271ef09ec3d6fd5c
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-03-22 16:54:57 +0530

    Added a view for hotel details

--- home/views.py
+++ home/views.py
@@ -10,3 +10,6 @@ def index(r):
         if sort_by == 'sort_low': hotels = hotels.order_by('hotel_offer_price')
         else: hotels = hotels.order_by('-hotel_offer_price')
     return render(r, "index.html", context={"hotels": hotels[:50]})
+
+def detail_hotel(r, slug):
+    return render(r, "details.html", context={"hotel": Hotel.objects.get(hotel_slug=slug)})
